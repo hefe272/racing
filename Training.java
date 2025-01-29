a4c90912fd96746c8900832bc81c9d19abc41079
hefe272 <felix-heuer@gmx.net>
2025-01-29 14:33:41 +0100

    Finished Administration

--- Training.java
+++ Training.java
@@ -3,10 +3,25 @@ import java.util.Date;
 
 public class Training extends Event{
 
+    public enum tot {Fußball,
+        Handball,
+        Volleyball,
+        Tischtennis,
+        Radball,
+        Turnen,
+        Dart,
+        Schach,
+        LeagueOfLegends,
+        Tanzen,
+        Basketball,
+        Pilates
+    };
 
+    public tot type;
 
-    Training(int pEventId, String pName, Date pDate, ArrayList<Member> pMembers) {
+    Training(int pEventId, String pName, Date pDate, ArrayList<Member> pMembers, tot ptype) {
         super(pEventId, pName, pDate, pMembers);
+        type = ptype;
     }
 }
 
